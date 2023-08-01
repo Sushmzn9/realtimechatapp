0fc1aa391d3245401d718b74fd728bc91e985597
Sushmzn9 <sushan.maharjan18@gmail.com>
2023-08-01 23:30:08 +1000

    update

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,5 +1,21 @@
 import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
 
 export const Chat = () => {
-  return <div>Chat</div>;
+  const chatProps = useMultiChatLogic(
+    process.env.REACT_APP_PROJECTS,
+    "testuser",
+    "1234",
+    ""
+  );
+  return (
+    <div style={{ flexBasis: "100%" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} />
+    </div>
+  );
 };
